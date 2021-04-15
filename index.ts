5882625702461c704402dacae884b2df4d931b61
williamandres <williamaguilar@misena.edu.co>
2021-04-14 22:18:57 -0500

    problem 2

--- index.ts
+++ index.ts
@@ -1,15 +1,14 @@
-const data = [
-    { name: "william", age: 18, math:30, spanish: 30, lireture: 30 },
-    { name: "santiago", age: 18, math:50, spanish: 30, lireture: 30 },
-    { name: "Alejandra", age: 18, math:30, spanish: 30, lireture: 30},
-    { name: "sandra", age: 18, math:30, spanish: 50, lireture: 30},
-    { name: "david", age: 18, math:30, spanish: 30, lireture: 30 }
-    ];
-
-    for(let i = 0; i < data.length; i++)
+for(let i = 1; i < 15; i++)
+{
+    if(i % 2 !== 0)
     {
-        if(data[i].math + data[i].spanish + data[i].lireture == 90)
-        {
-        console.log(data[i].name);
-        };
-    } 
+        continue;
+    }
+
+    if(i % 5 === 0 )
+    {
+        break;
+    }
+
+    console.log(i);
+}
