23239cf7ebfb5e9aadea8f20201217634f38bfb8
williamandres <williamaguilar@misena.edu.co>
2021-04-15 15:35:58 -0500

    prueba 3

--- index.js
+++ index.js
@@ -1,13 +1,5 @@
-var data = [
-    { name: "william", age: 18, math: 30, spanish: 30, lireture: 30 },
-    { name: "santiago", age: 18, math: 50, spanish: 30, lireture: 30 },
-    { name: "Alejandra", age: 18, math: 30, spanish: 30, lireture: 30 },
-    { name: "sandra", age: 18, math: 30, spanish: 50, lireture: 30 },
-    { name: "david", age: 18, math: 30, spanish: 30, lireture: 30 }
-];
-for (var i = 0; i < data.length; i++) {
-    if (data[i].math + data[i].spanish + data[i].lireture == 90) {
-        console.log(data[i].name);
-    }
-    ;
-}
+var base = 0;
+do {
+    base = Math.round(Math.random() * 10);
+    console.log(base);
+} while (base !== 6);
