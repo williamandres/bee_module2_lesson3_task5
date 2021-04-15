e372aa65cd392d00436ea033fc142452ded2f353
williamandres <williamaguilar@misena.edu.co>
2021-04-14 21:43:13 -0500

    problema 1

---
+++ index.ts
@@ -0,0 +1,15 @@
+const data = [
+    { name: "william", age: 18, math:30, spanish: 30, lireture: 30 },
+    { name: "santiago", age: 18, math:50, spanish: 30, lireture: 30 },
+    { name: "Alejandra", age: 18, math:30, spanish: 30, lireture: 30},
+    { name: "sandra", age: 18, math:30, spanish: 50, lireture: 30},
+    { name: "david", age: 18, math:30, spanish: 30, lireture: 30 }
+    ];
+
+    for(let i = 0; i < data.length; i++)
+    {
+        if(data[i].math + data[i].spanish + data[i].lireture == 90)
+        {
+        console.log(data[i].name);
+        };
+    } 
